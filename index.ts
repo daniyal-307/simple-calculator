c2fbed7154b27656afee34be25371ecde8bd9599
Daniyal Shahzad <daniyal-307@users.noreply.github.com>
2024-03-31 00:06:38 +0500

    pushing calculator to github

---
+++ index.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+
+do {
+    await askQuestion();
+    var again = await inquirer.prompt([
+        {
+            type: "list",
+            name: "restart",
+            choices: ["Yes", "No"],
+            message: chalk.gray("Do you want to continue?")
+        }
+    ])
+} while (again.restart == "Yes");
+
+
+
+async function askQuestion() {
+
+    let answer = await inquirer.prompt([
+       {
+            type: "list",
+            name: "operator",
+            message: chalk.gray("Which operation do you want to perform?"),
+            choices: ["+ Addition", "- Subtraction", "* Multiplication", "/ Division"]
+        }, {
+            type: "number",
+            name: "num1",
+            message: chalk.blueBright("Enter your first number:")
+        }, {
+            type: "number",
+            name: "num2",
+            message: chalk.blueBright("Enter your second number:"),
+        }
+    ])
+
+    switch (answer.operator) {
+        case "+ Addition": {
+            console.log(chalk.greenBright(`Your answer = ${answer.num1 + answer.num2}`));
+            break;
+        }
+        case "- Subtraction": {
+            console.log(chalk.greenBright(`Your answer = ${answer.num1 - answer.num2}`));
+            break;
+        }
+        case "* Multiplication": {
+            console.log(chalk.greenBright(`Your answer = ${answer.num1 * answer.num2}`));
+            break;
+        }
+        case "/ Division": {
+            console.log(chalk.greenBright(`Your answer = ${answer.num1 / answer.num2}`));
+            break;
+        }
+     }
+
+};
